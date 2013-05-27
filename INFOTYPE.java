8863b2d3b9976c68a8c18d0cd05057ef25e79b0d
RZineb <rihani.zineb@gmail.com>
2013-05-27 19:24:34 +0200

    Create INFOTYPE.java

---
+++ INFOTYPE.java
@@ -0,0 +1,14 @@
+
+
+public class INFOTYPE extends INFO {
+
+  public INFOTYPE(DTYPE t) {
+		super(t);
+		// TODO Auto-generated constructor stub
+	}
+	// affichage
+	public String toString() {
+		return "; ALIAS sur : " + "type=" + type ;
+	}
+
+}
